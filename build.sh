91ba7364444a5ad0ad95fd3510df021534a31be3
Aleksandr Panzin <alex@panz.in>
2016-05-03 18:53:21 +0100

    Java Version bump

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t jalexoid/ubuntu-java8:14.04-8u72 ./ubuntu-trusty-java8 
-docker build -t jalexoid/sonatype-nexus:3.0.0-m7 ./sonatype-nexus3
+docker build -t jalexoid/ubuntu-java8:14.04-8u92 ./ubuntu-trusty-java8 
+docker build -t jalexoid/sonatype-nexus:3.0.0-03 ./sonatype-nexus3
