f4e9e8b40f79697062b7db5d8443551c70a5bd89
Aleksandr Panzin <alex@panz.in>
2016-08-25 11:01:48 -0400

    Upgrade versions

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t jalexoid/ubuntu-java8:14.04-8u92 ./ubuntu-trusty-java8 
-docker build -t jalexoid/sonatype-nexus:3.0.0-03 ./sonatype-nexus3
+docker build -t jalexoid/ubuntu-java8:16.04-8u101 ./ubuntu-trusty-java8 
+docker build -t jalexoid/sonatype-nexus:3.0.1-01 ./sonatype-nexus3
