ac2bc99dbc8b51854adf5ce155bf584855a360cf
Aleksandr Panzin <alex@panz.in>
2017-07-17 13:37:55 -0400

    Updated Nexus version to 3.4.0-02 and Java 8u131

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t jalexoid/ubuntu-java8:16.04-8u111 ./ubuntu-xenial-java8
-docker build -t jalexoid/sonatype-nexus:3.1.0-04 ./sonatype-nexus3
-docker tag jalexoid/sonatype-nexus:3.1.0-04 jalexoid/sonatype-nexus:latest
+docker build -t jalexoid/ubuntu-java8:16.04-8u131 ./ubuntu-xenial-java8
+docker build -t jalexoid/sonatype-nexus:3.4.0-02 ./sonatype-nexus3
+docker tag jalexoid/sonatype-nexus:3.4.0-02 jalexoid/sonatype-nexus:latest
