a70088156a89466d688824417b9ebef92763ba0c
Aleksandr Panzin <alex@panz.in>
2017-07-17 11:59:35 -0400

    Updated Nexus version

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 docker build -t jalexoid/ubuntu-java8:16.04-8u111 ./ubuntu-xenial-java8
 docker build -t jalexoid/sonatype-nexus:3.1.0-04 ./sonatype-nexus3
+docker tag jalexoid/sonatype-nexus:3.1.0-04 jalexoid/sonatype-nexus:latest
